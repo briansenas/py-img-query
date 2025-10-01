e630b73c55dac88fd277541cccbcea41e7c396b3
briansenas <briansenasimons@gmail.com>
2025-10-01 19:35:58 +0200

    feat(selection): add buttons to cut the data
    NOTES: Select individual images is a WIP

--- app/pages/visualize.py
+++ app/pages/visualize.py
@@ -16,10 +16,22 @@ def load_data(json_file):
 
 
 st.title("Dynamic Image Filter")
+# Set env keys
+st_env_keys = [
+    "tmp_file",
+    "tmp_file_counter",
+]
+for env in st_env_keys:
+    if env not in st.session_state:
+        st.session_state[env] = 0
 
 json_file = st.file_uploader("Upload JSON file", type="json")
 if json_file is not None:
-    df = load_data(json_file)
+    if not st.session_state.tmp_file:
+        df = load_data(json_file)
+    else:
+        with open(st.session_state.tmp_file) as file:
+            df = load_data(file)
 
     # --- Filters Section ---
     st.sidebar.header("Build Filters")
@@ -33,12 +45,12 @@ if json_file is not None:
         col = st.sidebar.selectbox(
             f"Attribute {i+1}",
             options=df.columns,
-            key=f"col_{i}",
+            key=f"col_{i}_{st.session_state.tmp_file_counter}",
         )
         operator = st.sidebar.selectbox(
             f"Operator {i+1}",
             options=["AND", "OR"],
-            key=f"op_{i}",
+            key=f"op_{i}_{st.session_state.tmp_file_counter}",
         )
 
         # Filter UI
@@ -46,7 +58,7 @@ if json_file is not None:
             val = st.sidebar.radio(
                 f"Value for {col}",
                 options=[True, False],
-                key=f"val_{i}",
+                key=f"val_{i}_{st.session_state.tmp_file_counter}",
             )
             filters.append((col, val, operator))
         elif pd.api.types.is_numeric_dtype(df[col]):
@@ -55,17 +67,20 @@ if json_file is not None:
                 f"Min {col}",
                 value=min_val,
                 format="%.6f",
-                key=f"min_{i}_{col}",
+                key=f"min_{i}_{col}_{st.session_state.tmp_file_counter}",
             )
             max_input = st.sidebar.number_input(
                 f"Max {col}",
                 value=max_val,
                 format="%.6f",
-                key=f"max_{i}_{col}",
+                key=f"max_{i}_{col}_{st.session_state.tmp_file_counter}",
             )
             filters.append((col, (min_input, max_input), operator))
         else:
-            val = st.sidebar.text_input(f"Exact match for {col}", key=f"val_{i}")
+            val = st.sidebar.text_input(
+                f"Exact match for {col}",
+                key=f"val_{i}_{st.session_state.tmp_file_counter}",
+            )
             if val.strip():
                 filters.append((col, val, operator))
 
@@ -91,6 +106,45 @@ if json_file is not None:
         page_size = st.number_input("Images per page", 1, 50, 12)
         total_pages = max(1, (len(filtered_df) + page_size - 1) // page_size)
         page = st.number_input("Page", 1, total_pages, 1)
+        selection_cols = st.columns(2)
+        filter_selection = selection_cols[0].button(
+            "Set Selection",
+            type="primary",
+            use_container_width=True,
+        )
+        remove_selection = selection_cols[1].button(
+            "Remove Selection",
+            type="primary",
+            use_container_width=True,
+        )
+        if remove_selection or filter_selection:
+            filename = "data/tmp_file.json"
+            data_dump = df[~mask] if remove_selection else filtered_df
+            with open(filename, "w") as file:
+                file.write(json.dumps(data_dump.to_dict(orient="index"), indent=2))
+            st.session_state.tmp_file = filename
+            st.session_state.tmp_file_counter += 1
+            load_data.clear()
+            st.rerun()
+        download_cols = st.columns(2)
+        download_cols[0].download_button(
+            label="Download Selection",
+            data=json.dumps(filtered_df.to_dict(orient="index"), indent=2).encode(
+                "utf-8",
+            ),
+            file_name="filtered-images.json",
+            mime="application/json",
+            use_container_width=True,
+        )
+        download_cols[1].download_button(
+            label="Download Remainder",
+            data=json.dumps(df[~mask].to_dict(orient="index"), indent=2).encode(
+                "utf-8",
+            ),
+            file_name="filtered-images.json",
+            mime="application/json",
+            use_container_width=True,
+        )
 
     # Slice for current page
     start_idx = (page - 1) * page_size
